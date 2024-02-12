c5a322f93e40b95d2ebf30021335d8dc77655fe3
beardbaba <divyank596@gmail.com>
2024-02-12 21:38:44 +0530

    Finally completed v2

--- Day_9/blind_auction.py
+++ Day_9/blind_auction.py
@@ -9,10 +9,10 @@ print("Welcome to the secret auction program.")
 def intro():
     global name
     global bid
-    global another
+    global should_continue
     name = input("What is your name? : ")
     bid = int(input("What's your bid? : $"))
-    another = input("Are there any other bidders? Type 'yes' or 'no'. ").lower()
+    should_continue = input("Are there any other bidders? Type 'yes' or 'no'. ").lower()
     user(name,bid)
 
 def user(naam,daam):
@@ -23,7 +23,7 @@ def user(naam,daam):
 
 intro()
 
-while another == "yes":
+while should_continue == "yes":
     clear()
     intro()
 
@@ -38,4 +38,9 @@ print(f"The winner is {max_bid['name']} with a bid of ${max_bid['bid']}")
 """
  Came to the above logic of get_bid function and and max function using this stackoverflow solution and then chatgpt since didn't understand lambda
  https://stackoverflow.com/questions/5320871/how-to-find-the-min-max-value-of-a-common-key-in-a-list-of-dicts & https://chat.openai.com/share/0bc5135a-8dc6-423d-b1a7-aff534023147
+"""
+
+"""
+Angela's solution is also very simple and easy do check and make a note
+https://www.udemy.com/course/100-days-of-code/learn/lecture/19279420#questions/13418268
 """
