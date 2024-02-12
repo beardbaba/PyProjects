1076fc812cc42405cd6b335bcab5ba471e39c313
beardbaba <divyank596@gmail.com>
2024-02-12 21:20:12 +0530

    Finally completed

--- Day_9/blind_auction.py
+++ Day_9/blind_auction.py
@@ -3,27 +3,39 @@ def clear():
     os.system('cls')
 
 details = []    
-# other == "no"
 
 print("Welcome to the secret auction program.")
 
-name = input("What is your name? : ")
-bid = int(input("What's your bid? : $"))
-# another = input("Are there any other bidders? Type 'yes' or 'no'. ").lower()
+def intro():
+    global name
+    global bid
+    global another
+    name = input("What is your name? : ")
+    bid = int(input("What's your bid? : $"))
+    another = input("Are there any other bidders? Type 'yes' or 'no'. ").lower()
+    user(name,bid)
 
 def user(naam,daam):
-        details.append({
+    details.append({
             "name":naam,
             "bid":daam
         })
 
-# if other == yes:
-#     clear()
+intro()
 
-# clear()
+while another == "yes":
+    clear()
+    intro()
 
+def get_bid(amount):
+    return amount['bid']
 
-user(name,bid)
+max_bid = max(details, key = get_bid )
 
-print(f"The winner is {details[0]['name']} with a bid of ${details[0]['bid']}")
-print(details)
+print(f"The winner is {max_bid['name']} with a bid of ${max_bid['bid']}")
+# print(details)
+
+"""
+ Came to the above logic of get_bid function and and max function using this stackoverflow solution and then chatgpt since didn't understand lambda
+ https://stackoverflow.com/questions/5320871/how-to-find-the-min-max-value-of-a-common-key-in-a-list-of-dicts & https://chat.openai.com/share/0bc5135a-8dc6-423d-b1a7-aff534023147
+"""
